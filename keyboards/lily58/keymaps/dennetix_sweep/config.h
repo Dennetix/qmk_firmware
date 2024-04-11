9735682388cb10ae81ac3ce49f19fab0ff1c8bbd
Dennis Schöpf <dennisschoepf@proton.me>
2024-04-11 18:01:32 +0200

    higher tapping term

--- keyboards/lily58/keymaps/dennetix_sweep/config.h
+++ keyboards/lily58/keymaps/dennetix_sweep/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 165
+#define TAPPING_TERM 180
 
 #define ENTER_TAPPING_TERM 300
 
